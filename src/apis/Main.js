beb37c289f6ce52d369262065352baf0b32cc8c8
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-06-15 19:47:53 +0530

    .

--- src/apis/Main.js
+++ src/apis/Main.js
@@ -3,8 +3,6 @@ export const fetchTopRatedMovies = () => {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDUyNjRlYmI4ZTYyODVhYjlkYjQ1ZDdlYmVjZmM2YiIsInN1YiI6IjY2NmQ0OTU0NmI4YzQ3OTQ1YTM2MzVhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QjBCWACkZDu3OnmyGtfkhcD6oBs-eWez0VgPH7PR6w0',
     },
   };
 
