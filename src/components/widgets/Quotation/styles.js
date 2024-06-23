d79dd811e097e53012ba00d95868b77f16141a21
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-06-23 12:21:30 +0530

    Reanimated Added

--- src/components/widgets/Quotation/styles.js
+++ src/components/widgets/Quotation/styles.js
@@ -2,10 +2,14 @@ import {StyleSheet} from 'react-native';
 import {FONTS} from '../../../constants/Fonts';
 import {COLORS} from '../../../constants/Colors';
 import {fpx, vpx} from '../../../libraries/responsive-pixels';
-import {STD_HORIZONTAL_SPACING} from '../../../constants/Styles';
+import {
+  STD_HORIZONTAL_SPACING,
+  STD_VERTICAL_SPACING,
+} from '../../../constants/Styles';
 
 export const styles = StyleSheet.create({
   contentView: {
+    paddingVertical: STD_VERTICAL_SPACING,
     paddingHorizontal: STD_HORIZONTAL_SPACING,
     gap: vpx(8),
   },
