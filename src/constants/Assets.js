ac5ff2e0cffa64831edf4215c22af8ec5ece688a
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-07-15 13:46:57 +0530

    cleanup

--- src/constants/Assets.js
+++ src/constants/Assets.js
@@ -1,6 +1,3 @@
-export const PROFILE_IMAGE =
-  'https://media.licdn.com/dms/image/D5603AQEwgqk61oy06Q/profile-displayphoto-shrink_400_400/0/1713561898723?e=1723680000&v=beta&t=bvo0MwBiuhn4XpTHH0vWO5xr_VK6osWdSXn_KFtWseM';
-
 export const HAPPY_SPACEMAN_ANIM = require('@assets/animations/happy_spaceman.json');
 export const LOADING_CUBE_ANIM = require('@assets/animations/loading_cube.json');
 export const ERROR_404_PANDA_ANIM = require('@assets/animations/error_404_panda.json');
