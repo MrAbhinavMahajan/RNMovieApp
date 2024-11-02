aa3d7654c46e08acef6cc23544e8434e0efa239d
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-11-02 08:49:44 +0530

    back nav fix - Linking initialRouteName Added

--- src/constants/Linking.js
+++ src/constants/Linking.js
@@ -1,6 +1,7 @@
 export const LINKING_CONFIG = {
   prefixes: ['rn-movie-app://'],
   config: {
+    initialRouteName: 'MAIN_TAB',
     screens: {
       MOVIE_DETAILS_SCREEN: 'MOVIE_DETAILS_SCREEN/:payload',
     },
